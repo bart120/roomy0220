d79a0f62947db616368d81d1e3c5e080d8e88568
Vincent <vleclerc@inow.fr>
2020-02-25 12:25:54 +0100

    routage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 import { RoomsModule } from './pages/rooms/rooms.module';
 import { MaterialModule } from './material.module';
+import { APP_ROUTES } from './app.routes';
+import { HomeModule } from './pages/home/home.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { MaterialModule } from './material.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(APP_ROUTES),
     MaterialModule,
+    HomeModule,
     RoomsModule
   ],
   providers: [],
---
+++ src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+import { ListRoomsComponent } from './pages/rooms/list-rooms/list-rooms.component';
+import { CreateRoomComponent } from './pages/rooms/create-room/create-room.component';
+import { HomeComponent } from './pages/home/home/home.component';
+import { NotFoundComponent } from './pages/home/not-found/not-found.component';
+
+export const APP_ROUTES: Routes = [
+
+    /*{ path: 'rooms/list', component: ListRoomsComponent },
+    { path: 'rooms/create', component: CreateRoomComponent },*/
+    {
+        path: 'rooms', children: [
+            { path: 'list', component: ListRoomsComponent },
+            { path: 'create', component: CreateRoomComponent }
+        ]
+    },
+    { path: 'home', component: HomeComponent },
+    { path: 'notfound', component: NotFoundComponent },
+
+    //{ path: 'auth/login', component: LoginComponent }
+    { path: 'auth', loadChildren: () => import('./pages/authentication/authentication.module').then(x => x.AuthenticationModule) },
+
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', redirectTo: 'notfound' }
+];
